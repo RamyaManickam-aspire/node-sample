dd53d85df18d15b5a96bccf4c869cac34f0235b3
RamyaManickam-aspire <ramya.manickam@aspiresys.com>
2024-08-27 14:46:32 +0530

    initia

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 import express from "express";
+import pg from "pg";
+import { Connector } from "@google-cloud/cloud-sql-connector";
+
+const { Pool } = pg;
+
+const connector = new Connector();
+const clientOpts = await connector.getOptions({
+  instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME,
+  authType: "IAM",
+});
+
+const pool = new Pool({
+  ...clientOpts,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+
 const app = express();
 
-app.get("/", (req, res) => {
-  const name = process.env.NAME || "World";
-  res.send(`Hello ${name}!`);
+app.get("/", async (req, res) => {
+  await pool.query("INSERT INTO visits(created_at) VALUES(NOW())");
+  const { rows } = await pool.query(
+    "SELECT created_at FROM visits ORDER BY created_at DESC LIMIT 5"
+  );
+  console.table(rows);
+  res.send(rows);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
-app.listen(port, () => {
+app.listen(port, async () => {
+  //   console.log("process.env: ", process.env);
+  //   await pool.query(`CREATE TABLE IF NOT EXISTS visits (
+  //     id SERIAL NOT NULL,
+  //     created_at timestamp NOT NULL,
+  //     PRIMARY KEY (id)
+  //   );`);
   console.log(`helloworld: listening on port ${port}`);
 });
+// import express from "express";
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   const name = process.env.NAME || "World";
+//   res.send(`Hello ${name}!`);
+// });
+
+// const port = parseInt(process.env.PORT) || 8080;
+// app.listen(port, () => {
+//   console.log(`helloworld: listening on port ${port}`);
+// });
